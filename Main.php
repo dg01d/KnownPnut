ca601299e25600557d36511ee7916e7d080883f6
Daniel Goldsmith <dgold@ascraeus.org>
2017-03-13 18:06:31 +0000

    requestbin was supposed to help

--- Main.php
+++ Main.php
@@ -248,7 +248,7 @@ namespace IdnoPlugins\Pnut {
 				$entity->text = $status;
 
 				$entity->annotations = $attachment_list;
-				$result = \Idno\Core\Webservice::post('http://requestb.in/16ekvrw1'. json_encode($entity));
+				$result = \Idno\Core\Webservice::post('http://requestb.in/16ekvrw1?include_annotations=1&'. json_encode($entity));
 				//$result = \Idno\Core\Webservice::post('https://api.pnut.io/v0/posts?include_annotations=1&access_token=' . $pnutAPI->access_token, json_encode($entity), ['Content-Type: application/json']);
 				$content = json_decode($result['content']);
 				
