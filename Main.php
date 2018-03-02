6afac4c0bc0a08e9e4ae9555acd35847ead02f4a
33MHz <ing@codespelunkers.com>
2018-03-01 22:50:58 -0600

    fix #4, add https://github.com/idno/Twitter/blob/master/Main.php#L64 (though not working), move token to header, minor improvements

--- Main.php
+++ Main.php
@@ -66,6 +66,19 @@ namespace IdnoPlugins\Pnut {
 	    }, ['note', 'article', 'image']);
 
 
+        // Activate syndication automatically, if replying to twitter
+        \Idno\Core\Idno::site()->addEventHook('syndication/selected/pnut', function (\Idno\Core\Event $event) {
+            $eventdata = $event->data();
+            if (!empty($eventdata['reply-to'])) {
+                $replyto = (array) $eventdata['reply-to'];
+                foreach ($replyto as $url) {
+                    if (strpos(parse_url($url)['host'], 'pnut.io')!==false)
+                        $event->setResponse(true);
+                }
+            }
+        });
+
+
 	    // Push "notes" to Pnut
 	    \Idno\Core\site()->addEventHook('post/note/pnut', function(\Idno\Core\Event $event) {
 
@@ -75,33 +88,35 @@ namespace IdnoPlugins\Pnut {
 			$pnutAPI->setAccessToken(\Idno\Core\site()->session()->currentUser()->pnut['access_token']);
 			$message = strip_tags($object->getDescription());
 
-			if (!empty($message) && substr($message, 0, 1) != '@') {
+			if (strlen($message) != 0 && substr($message, 0, 1) != '@') {
 
 			    try {
 
 				$entity = new \stdClass();
 				$entity->text = $message;
-				/* Per @33mhz, Pnut doesn't need this. 
-				$entity->entities = $this->getEntities($message); 
-				$entity->parse_links = true;     
-				*/
+				// single post created on pnut will be a reply to the last pnut.io reply given
+				if (count($object->inreplyto)) {
+					foreach($object->inreplyto as $url) {
+						$parsed = parse_url($url);
+						if (isset($parsed['host']) && (strtolower($parsed['host'])=='posts.pnut.io' || strtolower($parsed['host'])=='beta.pnut.io') && ($pieces = explode('/',$parsed['path'])) && is_numeric($pieces[count($pieces)-1])) {
+							$entity->reply_to = $pieces[count($pieces)-1];
+						}
+					}
+				}
 
 				$attachment_list = [];
 				$cross = new \stdClass();
 				$cross->type = 'io.pnut.core.crosspost';
 				$cross->value = new \stdClass();
 				$cross->value->canonical_url = $object->getUrl();
-				//$cross->value->text = $status;
+
 				$attachment_list[] = $cross;
 				$entity->raw = $attachment_list;
 
-				$result = \Idno\Core\Webservice::post('https://api.pnut.io/v0/posts?access_token=' . $pnutAPI->access_token, json_encode($entity /*[
-					    'text' => $message,
-					    'entities' => $this->getEntities($message)
-				]*/), ['Content-Type: application/json']);
+				$result = \Idno\Core\Webservice::post('https://api.pnut.io/v0/posts', json_encode($entity), ['Authorization: Bearer ' . $pnutAPI->access_token,'Content-Type: application/json']);
 				$content = json_decode($result['content']);
 
-				if ($result['response'] < 400) {
+				if ($result['response'] == 201) {
 				    // Success
 				    $id = $content->data->id; 
 				    $user = $content->data->user->username; // Think this gets user id
@@ -132,20 +147,17 @@ namespace IdnoPlugins\Pnut {
 			    $desc = html_entity_decode(strip_tags($object->getDescription()));
 			    $parse = parse_url($object->getURL());
 			    $domain = $parse['host'];
-			    $domlen = (strlen($domain) + 3);
+			    $domlen = (mb_strlen($domain,'UTF-8') + 3);
 
 			    $post = $status . ': ' . $desc;
 			    $boundary = (256 - $domlen);
-			    $cutoff = ($boundary - 4);
+			    $cutoff = ($boundary - 2);
 
-			    if (strlen($post) > $boundary) { // Trim status down if required
-				$post = substr($post, 0, $cutoff) . '... ';
+			    if (mb_strlen($post,'UTF-8') > $boundary) { // Trim status down if required
+				$post = mb_substr($post, 0, $cutoff) . '… ';
 			    }
-			    /*
-			    $statlen = strlen($status);
 
-				*/
-			    $article = $post . ' [[' . $domain . '](' . $object->getURL() . ')]';
+			    $article = $post . ' [' . $domain . '](' . $object->getURL() . ')';
 
 			    /* Attachment crosspost for pnut as used by patter */ 
 			    $attachment_list = []; 
@@ -157,13 +169,7 @@ namespace IdnoPlugins\Pnut {
 			    /**/
 			    $entity = new \stdClass();
 			    $entity->text = $article; 
-			    /* 
-			    $entity->entities = $this->getEntities($status);
-			    */
 			    $entity->raw = $attachment_list;
-			    /*
-			    $entity->parse_links = true; Differing API?  
-			    */
 			    
 			    $result = \Idno\Core\Webservice::post('https://api.pnut.io/v0/posts?access_token=' . $pnutAPI->access_token, json_encode($entity /*[
 					'text' => $status,
@@ -172,7 +178,7 @@ namespace IdnoPlugins\Pnut {
 			    ]*/), ['Content-Type: application/json']);
 			    $content = json_decode($result['content']);
 
-			    if ($result['response'] < 400) {
+			    if ($result['response'] == 201) {
 				// Success
 			    $id = $content->data->id;               // This gets the post id
 			    $user = $content->data->user->username; // Think this gets user id
@@ -273,7 +279,7 @@ namespace IdnoPlugins\Pnut {
 				$result = \Idno\Core\Webservice::post('https://api.pnut.io/v0/posts?include_raw=1&access_token=' . $pnutAPI->access_token, json_encode($entity), ['Content-Type: application/json']);
 				$content = json_decode($result['content']);
 				
-				if ($result['response'] < 400) {
+				if ($result['response'] == 201) {
 				    // Success
 				    $id = $content->data->id;               // This gets user id
 				    $user = $content->data->user->username; // Think this gets user id
